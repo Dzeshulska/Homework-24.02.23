cb717f62d6a6458743b77dd342372467dfbcad89
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-25 10:14:14 +0200

    added new file

---
+++ homework/task1.py
@@ -0,0 +1,3 @@
+user_name = input("enter your name: ")
+print(user_name.lower().capitalize())
+
---
+++ homework/task3.py
@@ -0,0 +1,3 @@
+user_age = int(input("Enter how old are you?: "))
+year_of_birth = 2023- user_age
+print(year_of_birth)
