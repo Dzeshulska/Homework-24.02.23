ea4b0bd0abe01adf02f4d000a209781261be62b7
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-25 10:43:30 +0200

    added changes

---
+++ homework/task4.py
@@ -0,0 +1,20 @@
+user = user_age = int(input("Enter how old are you?"))
+
+print("<13", user_age <13)
+print(">13", user_age >13)
+print("<=13", user_age <=13)
+print(">=13", user_age >=13)
+print("==13", "13")
+
+print("<15", user_age <15)
+print(">15", user_age >15)
+print("<=15", user_age <=15)
+print(">=15", user_age >=15)
+print("==15", "15")
+
+print("<23", user_age <23)
+print(">23", user_age >23)
+print("<=23", user_age <=23)
+print(">=23", user_age >=23)
+print("==23", "23")
+
