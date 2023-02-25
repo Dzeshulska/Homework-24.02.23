1c09d892dc32d02a59cc28f4025c3ed86095eb23
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-25 10:47:25 +0200

    added changes in file

---
+++ homework/task5.py
@@ -0,0 +1,27 @@
+expression = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium nihil
+laborum rerum ipsum iure dicta officiis, necessitatibus non vel excepturi minima
+quia fugiat hic numquam suscipit harum laboriosam magni quasi veniam voluptas
+eius sapiente aperiam vero? Eum ducimus consequuntur, accusamus tempore corporis
+numquam animi hic ad voluptatem saepe mollitia voluptate explicabo soluta earum!
+Consequatur porro eos minus facilis mollitia alias nesciunt. Molestias explicabo
+alias ea praesentium placeat ex ad maxime ipsam non velit architecto, iure
+laborum reiciendis pariatur do lorem amet nulla dolor quos a esse atque vel? Fuga
+quidem perspiciatis velit iure excepturi. Delectus velit amet distinctio error
+temporibus at voluptas suscipit laboriosam rerum ea, quibusdam nesciunt maiores
+quas necessitatibus fugit veniam, molestiae inventore dignissimos voluptatum
+libero enim natus! Cum, corrupti ad."""
+
+print(len(expression))
+  
+print("ad" in expression)
+
+print(expression.find("ad"))
+
+print(expression[39:41])
+
+
+print(len(expression))
+ 
+print(expression.find("Lorem"))
+
+print(expression.replace("Lorem", "hey you"))
